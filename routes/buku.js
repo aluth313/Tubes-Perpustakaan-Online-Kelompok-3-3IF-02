882b44e3d73bfe42df6d403ed49f9761998cd6c8
Gilang Krisma Wijaya <gilangkrisma52@gmail.com>
2020-06-28 21:06:41 +0700

    Crud Buku

--- routes/buku.js
+++ routes/buku.js
@@ -22,19 +22,70 @@ router.post('/buku/tambah', (req, res) => {
     Buku
         .create(data)
         .then((buku) => {
-            res.render('sites/admin/master/buku/tambah', { data: data });
+            res.redirect('/admin/buku');
         })
         .catch((error) => {
             console.log(error);
         })
 });
 
+//update
+router.post('/buku/update/:id', (req, res) => {
+    Buku
+        .update({
+            judul: req.body.judulBuku,
+            penerbit: req.body.penerbitBuku,
+            pengarang: req.body.pengarangBuku,
+            tahun: req.body.tahunBuku,
+            isbn: req.body.isbnBuku
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then((buku) => {
+            res.redirect('/admin/buku');
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+});
+
+//index
 router.get('/buku', (req, res) => {
     Buku
         .findAll()
         .then((buku) => {
             let data = buku;
             res.render('sites/admin/master/buku/buku', { data: data });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+});
+
+//edit
+router.get('/buku/:id', (req, res) => {
+    Buku
+        .findByPk(req.params.id)
+        .then((buku) => {
+            let data = buku;
+            res.render('sites/admin/master/buku/edit', { data: data });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+});
+
+// delete
+router.get('/buku/destroy/:id', (req, res) => {
+    Buku.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(() => {
+            res.redirect('/admin/buku');
         })
         .catch((error) => {
             console.log(error);
