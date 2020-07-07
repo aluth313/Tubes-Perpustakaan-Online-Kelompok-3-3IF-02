52822890eea78d5c77a4bb1cc0065d61966eadd1
Ahmad Luthfi Nasrulloh <luthfiahmad36@gmail.com>
2020-07-07 14:49:49 +0700

    peminjaman

--- routes/peminjaman.js
+++ routes/peminjaman.js
@@ -2,42 +2,104 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 const Peminjaman = models.Peminjaman;
+const Buku = models.Buku;
+const { Op } = require("sequelize");
 
-router.get('/peminjaman/tambah', (req, res) => {
-    res.render('sites/admin/master/peminjaman/tambah', { data: null });
+router.get('/peminjaman', async (req, res) => {
+    if (typeof req.session.loggedin !== 'undefined') {
+        let booking = await Peminjaman.findAll({
+            where: {
+                status: 'booking'
+            },
+            include: ['anggota','buku']
+        });
+
+        let pinjam = await Peminjaman.findAll({
+            where: {
+                status: {
+                    [Op.notIn]: ['booking']
+                }
+            },
+            include: ['anggota','buku']
+        });
+        res.render('sites/admin/master/peminjaman/peminjaman', { data: pinjam, booking: booking });
+    } else {
+        res.redirect('/login')
+    }
 });
 
-router.post('/peminjaman/tambah', (req, res) => {
-    let data = {
-        tanggal_peminjaman: req.body.pemPinjam,
-        tanggal_pengembalian: req.body.pengPinjam,
-        status: req.body.statusPinjam,
-        nip: req.body.nipPinjam,
-        isbn: req.body.isbnPinjam,
-        no_registrasi: req.body.regPinjam
-
-    };
-
-    Peminjaman
-        .create(data)
+//terima
+router.post('/peminjaman/terima/:id', (req, res) => {
+    if (typeof req.session.loggedin !== 'undefined') {
+        Peminjaman.findByPk(req.params.id)
         .then((peminjaman) => {
-            res.render('sites/admin/master/peminjaman/tambah', { data: data });
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            Peminjaman.update({
+                status: 'dipinjam'
+            }, {
+                where: {
+                    id: peminjaman.id
+                }
+            });
+            res.redirect('/admin/peminjaman')
+        });
+    } else {
+        res.redirect('/login')
+    }
 });
 
-router.get('/peminjaman', (req, res) => {
-    Peminjaman
-        .findAll()
-        .then((peminjamn) => {
-            let data = peminjamn;
-            res.render('sites/admin/master/peminjaman/peminjaman', { data: data });
+//tolak
+router.get('/peminjaman/tolak/:id', async (req, res) => {
+    if (typeof req.session.loggedin !== 'undefined') {
+        let peminjaman = await Peminjaman.findByPk(req.params.id);
+        Buku.findByPk(peminjaman.buku_id)
+        .then((buku) => {
+            Buku.update({
+                jumlah: (buku.jumlah + 1)
+            }, {
+                where: {
+                    id: buku.id
+                }
+            });
+        });
+        Peminjaman.destroy({
+            where: {
+                id: req.params.id
+            }
         })
-        .catch((error) => {
-            console.log(error);
-        })
+        res.redirect('/admin/peminjaman')
+    } else {
+        res.redirect('/login')
+    }
+});
+
+//kembali
+router.post('/peminjaman/kembali/:id', async (req, res) => {
+    if (typeof req.session.loggedin !== 'undefined') {
+        let idBuku = await Peminjaman.findByPk(req.params.id);
+        Peminjaman.findByPk(req.params.id)
+        .then((peminjaman) => {
+            Peminjaman.update({
+                status: 'selesai'
+            }, {
+                where: {
+                    id: peminjaman.id
+                }
+            });
+        });
+        Buku.findByPk(idBuku.buku_id)
+        .then((buku) => {
+            Buku.update({
+                jumlah: (buku.jumlah + 1)
+            }, {
+                where: {
+                    id: buku.id
+                }
+            });
+        });
+        res.redirect('/admin/peminjaman')
+    } else {
+        res.redirect('/login')
+    }
 });
 
 module.exports = router
