c744d244cdd4a45db18380ef9853e1c8b3901dfb
Muhammad Taufik <muhamadtauf@gmail.com>
2020-07-08 13:47:42 +0700

    unduh ebook, menu ebook umum

--- routes/umumEbook.js
+++ routes/umumEbook.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
+const Ebook = models.Ebook;
 const upload = require('express-fileupload');
 
 
 //index
 router.get('/ebook', (req, res) => {
-        res.render('sites/umum/master/ebook/ebook');
-   });
+        Ebook
+    .findAll()
+    .then((ebook) => {
+        let data = ebook;
+        res.render('sites/umum/master/ebook/ebook', { data: data });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+   
+});
+
+
+//download
+router.get('/ebook/download/:id', (req, res) => {
+             
+            Ebook
+         .findByPk(req.params.id)
+         .then((ebook) => {
+            let data = ebook;
+            var filePath = "/../public/ebook/"; // Or format the path using the `id` rest param
+            var fileName = data.file; // file name
+            console.log(fileName); 
+            res.download(__dirname +filePath + fileName, fileName);
+         })
+         .catch((error) => {
+             console.log(error);
+         })
+      
+      
+     });
 
 module.exports = router
